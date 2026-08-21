97504ec054edc81cdc966772ce69ef54c421c433
arpit-sonar <arpitsonar12@gmail.com>
2026-08-21 17:51:53 +0530

    Fixed CORS policy with wildcard

--- Model-3-Predictive-Engine/Pricing-Backend/main.py
+++ Model-3-Predictive-Engine/Pricing-Backend/main.py
@@ -6,14 +6,10 @@ import pandas as pd
 
 app =  FastAPI(title="Dynamic Parking Pricing API")
 
-origins = [
-    "https://predictive-pricing-dashboard-chat-app6.vercel.app", 
-    "http://localhost:5173",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins, 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
